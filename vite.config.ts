5e54f829a845d741d00875f1b0d3d1c283363049
chris-88 <chris@l1.ie>
2025-03-28 13:04:29 +0000

    Adding publish to GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// Replace with your repo name
+const repoName = "pulse-security"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: `/${repoName}/`,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
